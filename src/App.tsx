0e2a27da34ee213569bbfbc472858503d43518e2
Manikanta Swamy <20pa1a5706@vishnu.edu.in>
2025-05-26 15:25:11 +0000

    added map and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages";
 import MainLayout from "./layout/MainLayout";
+import Locations from "./pages/Locations";
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
-          {/* <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} /> */}
+          <Route path="/locations" element={<Locations />} />
+          {/* <Route path="/contact" element={<ContactPage />} /> */}
         </Route>
       </Routes>
     </BrowserRouter>
--- src/pages/Locations.tsx
+++ src/pages/Locations.tsx
@@ -1,5 +1,65 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import wendingMachine from "../assets/wendingMachine.png";
+
+// Fix default icon path issue in Leaflet
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+});
+
+// Custom vending machine icon
+const vendingMachineIcon = new L.Icon({
+  iconUrl: wendingMachine,
+  iconSize: [38, 38], // Size of the icon
+  iconAnchor: [19, 38], // Point of the icon which corresponds to marker's location
+  popupAnchor: [0, -38], // Where the popup should open relative to the iconAnchor
+});
+
+const cities = [
+  { name: "Vizag", position: [17.6868, 83.2185] },
+  { name: "Hyderabad", position: [17.385, 78.4867] },
+  { name: "Bengaluru", position: [12.9716, 77.5946] },
+  { name: "Goa", position: [15.2993, 74.124] },
+];
+
 const Locations = () => {
-  return <div></div>;
+  return (
+    <div className="w-full flex h-full flex-col pt-16">
+      {/* Description */}
+      <div className="text-center text-gray-700 py-8 px-20">
+        <p className="text-lg leading-relaxed">
+          We serve key cities like Bengaluru, Hyderabad, Vizag, and Goa.
+          Wherever you are, we provide reliable and easy-to-use solutions
+          tailored for you. Connect with us and experience the best service.
+        </p>
+      </div>
+      <MapContainer
+        center={[15.0, 78.0]}
+        zoom={6}
+        scrollWheelZoom={true}
+        className="w-4/6 h-4/6   self-center"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            key={city.name}
+            position={city.position}
+            icon={vendingMachineIcon}
+          >
+            <Popup>{city.name}</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
 };
 
 export default Locations;
